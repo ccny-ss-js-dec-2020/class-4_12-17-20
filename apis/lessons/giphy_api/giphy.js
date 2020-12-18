31a00ff35d20f127be0e670f40fa4e5952576bcf
Jared Thomas <jaredthomas@Jareds-MacBook-Air.local>
2020-12-17 20:59:05 -0500

    adding updated class lesson and exercise solved movies

--- apis/lessons/giphy_api/giphy.js
+++ apis/lessons/giphy_api/giphy.js
@@ -6,7 +6,8 @@
 	* It should be activated right away
 	* The giphy api url looks like this when calling it:
 	  * http://api.giphy.com/v1/gifs/search?q=<Search Term>&api_key=<API Key>&limit=<Number Of Results>;
-	  * Where you see the <>, those will be input by you
+		* an example would be http://api.giphy.com/v1/gifs/search?q=bobcats&api_key=dsaf2khkdC&limit=3
+		* Where you see the <>, those will be input by you
 		* See the example below
 
 */
@@ -40,13 +41,14 @@ if(process.argv.length == 2){
 	request(giphyApiUrl, function(error, response, body) {
 		// logging the response from the giphy api call
 		console.log("this is the response object that we get when calling the giphy api")
-		console.log(response)
+		//console.log(response)
 		// if no error and the status code is equal to 200
 		// here, were are accessing the statusCode value from the response object
 		if (!error && response.statusCode === 200) {
 			// then here is the successful response to the giphy api, but you see it's a weird string because its unparsed
 			console.log("unparsed body from the giphy api")
-			console.log(body);
+			// look how ugly it is. Always use JSON.parse
+			//console.log(body);
 			console.log("parsed body from the giphy api")
 			// JSON.parse is used to be able to have your logic be able to read the properties and values from the object
 			// without JSON.parse here, you would have a difficult time reading through the response object
@@ -60,6 +62,7 @@ if(process.argv.length == 2){
 			// ... JSON.parse(res.body) will turn that string into an object that we are able to easily parse through
 			console.log("parsed response body")
 			var apiResponse = JSON.parse(response.body);
+			console.log(apiResponse)
 			// creating the custom error response to send to the user
 			var customError = "StatusCode: " + response.statusCode + ", Status Message: " + apiResponse.message;
 			// throwing the error
@@ -71,11 +74,12 @@ if(process.argv.length == 2){
 /* Searching with custom input via process.argv */
 
 //if there is a third argument in the terminal
-if(process.argv.length == 3){
+if(process.argv.length >= 3){
 
 	//getting the search term from the terminal command that you ran
 	//by getting everything from the 2nd argument and on (everything after node giphy.js)
 	var giphySearchTermForUrl = process.argv.slice(2).join('+');
+	console.log(giphySearchTermForUrl)
 	//api url with inputs from above
 	if(giphySearchTermForUrl){
 
@@ -86,20 +90,31 @@ if(process.argv.length == 3){
 		request(giphyApiUrl, function(error, response, body) {
 			// logging the response from the giphy api call
 			console.log("this is the response object that we get when calling the giphy api")
-			console.log(response)
+			/* log this if you want, its ugly */
+			// console.log(response)
 			// if no error and the status code is equal to 200
 			// here, were are accessing the statusCode value from the response object
 			if (!error && response.statusCode === 200) {
 				// then here is the successful response to the giphy api, but you see it's a weird string because its unparsed
 				console.log("unparsed body from the giphy api")
-				console.log(body);
+				/* log this if you want, its ugly */
+				// console.log(body);
 				console.log("parsed body from the giphy api")
 				// JSON.parse is used to be able to have your logic be able to read the properties and values from the object
 				// without JSON.parse here, you would have a difficult time reading through the response object
 				console.log(JSON.parse(body))
+				// this here is getting all of the images for the first record returned from the response
+				//console.log(JSON.parse(body).data[0].images)
+
+				// data is the array that holds all of image data, so you'd have to loop through the data array to get all of the images for all of the results
+				var dataArray = JSON.parse(body).data;
+				for(var i = 0; i < dataArray.length; i++){
+				//	console.log(dataArray[i].images)
+				}
 			} else {
 				// if an error, then show the error in the console
-				console.log(response)
+				/* log this if you want, its ugly */
+				//console.log(response)
 				// here is the parsed joke response from the joke rapid api
 				// JSON.parse is used to be able to easily access fields from a response object or any json object
 				// in this case, the object comes in as a string from the rapid api, so ...
